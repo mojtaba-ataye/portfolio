14a3275648f84197f9e16291a60d0bca559d483d
Mojtaba Ataye <mojtaba.ataye.dev@gmail.com>
2026-02-06 17:32:30 +0430

    remove projects and enable experienece

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Experience from "./components/Experience";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Experience from './components/Experience';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -15,8 +15,8 @@ function App() {
         <Hero />
         <About />
         <Skills />
-        <Projects />
-        {/* <Experience /> */}
+        {/* <Projects /> */}
+        <Experience />
         <Contact />
       </main>
       <Footer />
